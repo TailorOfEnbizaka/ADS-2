ab855653f53a098fd8a08c05df8105e9f81963b4
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-05-12 17:49:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double result = value;
+  if (n == 0) {
+    return 1;
+  } else {
+  for (int i = 1; i < n; i++) {
+    result *= value;
+  }
+  return result;
+}
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n <= 1)
+    return 1;
+  else
+    return (n* fact(n - 1));
 }
 
 double calcItem(double x, uint16_t n) {
-
+return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  double result = 1;
+  for (int n = 1; n <= count; n++) {
+    result += calcItem(x, n);
+  }
+  return result;
 }
 
-double sinn(double x, uint16_t count) {
-
+double sinn(double x, uint16_t count)  {
+  double result = x, k = 1;
+  for (int n = 2; n <= count; n++) {
+    result += (pown(-1.0, n-1) * calcItem(x, 2*n-1));
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double result = 1, k = 1;
+  for (int n = 2; n <= count; n++) {
+    result += (pown(-1.0, n-1) * calcItem(x, 2*n-2));
+  }
+  return result;
 }
